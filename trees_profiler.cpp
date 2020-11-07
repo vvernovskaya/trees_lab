70567972d7b601f228d2e2bf5f49be5b3b32dc07
Vika <vvernovskaya@gmail.com>
2020-11-08 00:49:22 +0300

    test insert, erase and find

--- trees_profiler.cpp
+++ trees_profiler.cpp
@@ -4,10 +4,13 @@
 #include <random>
 #include "RBTree.h"
 #include <set>
+#include <string>
 #include <iomanip>
 #include <fstream>
 
-//include tree headers
+const int NUM_NUM = 2000;
+const int NUM_TEST_ONE = 5000;
+const int RAND_RANGE = 1000000;
 
 using std::vector;
 
@@ -54,66 +57,109 @@ public:
         }
     }
 
-    void test_insert() {
+    void call_test() {
        for (int i_sz = 0; i_sz < tree_sizes.size(); i_sz++) {
            while (test_tree.size() < tree_sizes[i_sz]) {
                std::cout << "Choosing random number..." << '\n';
                std::cout << "-- Seeking tree_size = " << tree_sizes[i_sz] << '\n';
                std::cout << "---- Before inserting test_tree size = " << test_tree.size() << '\n';
-               test_tree.insert(rand_num(-100000, 100000));
+               test_tree.insert(rand_num(-RAND_RANGE, RAND_RANGE));
                std::cout << "---- After inserting test_tree size = " << test_tree.size() << '\n';
-               std::cout << " Chose a random number." << '\n';
+               std::cout << "Chose a random number." << '\n';
            }
            std::cout << "Working on element " << tree_sizes[i_sz] << " out of " << tree_sizes[tree_sizes.size()-1] << '\n';
-           test_insert_local();
+           test_local();
        }
     }
-    void test_erase() {
-        return;
-    }
-    void test_find() {
-        return;
-    }
 
-    void test_insert_local() {
-        double avg_time_per_size = 0;
-        for (int i = 0; i < 100; i++) {
-            double avg_time_per_num = 0;
-            int32_t curr_num = rand_num(-100000, 100000);
+    void test_local() {
+        double avg_time_per_size_insert = 0;
+        double avg_time_per_size_find = 0;
+        double avg_time_per_size_erase = 0;
+
+        for (int i = 0; i < NUM_NUM; i++) {
+            double avg_time_per_num_insert = 0;
+            double avg_time_per_num_find = 0;
+            double avg_time_per_num_erase = 0;
+
+            int32_t curr_num = rand_num(-RAND_RANGE, RAND_RANGE);
             std::cout << "---- Chose number " << curr_num << ". Starting averaging..." << '\n';
 
-            for (int j = 0; j < 300; j++) {
-                double time_start = get_time();
-                test_tree.insert(curr_num);
-                double time_finish = get_time();
+            for (int k = 0; k < NUM_TEST_ONE; k++) {
+                double time_start_find = get_time();
+                bool find = (test_tree.find(curr_num) == test_tree.end());
+                double time_finish_find = get_time();
 
-                avg_time_per_num += time_finish - time_start;
-                test_tree.erase(curr_num);
+                avg_time_per_num_find += time_finish_find - time_start_find;
             }
-            avg_time_per_num = double( (double) avg_time_per_num / 300);
-            avg_time_per_size += avg_time_per_num;
+            avg_time_per_num_find = double( (double) avg_time_per_num_find / NUM_TEST_ONE);
+
+            for (int j = 0; j < NUM_TEST_ONE; j++) {
+                double time_start_insert = get_time();
+                test_tree.insert(curr_num);
+                double time_finish_insert = get_time();
+                avg_time_per_num_insert += time_finish_insert - time_start_insert;
+
+                double time_start_erase = get_time();
+                test_tree.erase(curr_num);
+                double time_finish_erase = get_time();
+                avg_time_per_num_erase += time_finish_erase - time_start_erase;
+            }
+
+            avg_time_per_num_insert = double( (double) avg_time_per_num_insert / NUM_TEST_ONE);
+            avg_time_per_num_erase = double( (double) avg_time_per_num_erase / NUM_TEST_ONE);
+
+            avg_time_per_size_insert += avg_time_per_num_insert;
+            avg_time_per_size_erase += avg_time_per_num_erase;
+
             std::cout << "---- Finished averaging" << '\n';
         }
-        avg_time_per_size = double( (double) avg_time_per_size / 100);
-        times_insert.push_back(avg_time_per_size);
+        avg_time_per_size_insert = double( (double) avg_time_per_size_insert / NUM_NUM);
+        avg_time_per_size_find = double( (double) avg_time_per_size_find / NUM_NUM);
+        avg_time_per_size_erase = double( (double) avg_time_per_size_erase / NUM_NUM);
+
+        times_insert.push_back(avg_time_per_size_insert);
+        times_find.push_back(avg_time_per_size_find);
+        times_erase.push_back(avg_time_per_size_erase);
     }
 
-    void print_insert_table() {
+    void make_table(vector<double> times, std::string file_name) {
         std::cout << "-- Started making a CSV... ";
         std::ofstream f;
-        f.open("output_data_2.csv");
+        f.open(file_name);
         for (int i = 0; i < tree_sizes.size(); i++) {
             f << tree_sizes[i] << ",";
-            f << std::setprecision(3) << times_insert[i] << '\n';
+            f << std::setprecision(6) << times[i] << '\n';
         }
         std::cout << "finished CSV" << '\n';
+    }
+
+    void test_insert() {
+        call_test();
+        make_table(times_insert, "output_times_insert.csv");
+    }
+
+    void test_find() {
+        call_test();
+        make_table(times_find, "output_times_find.csv");
+    }
+
+    void test_erase() {
+        call_test();
+        make_table(times_erase, "output_times_erase.csv");
+    }
+
+    void test_all() {
+        call_test();
+        make_table(times_insert, "output_times_insert.csv");
+        make_table(times_find, "output_times_find.csv");
+        make_table(times_erase, "output_times_erase.csv");
     }
 };
 
 int main() {
     Profiler<std::set<int32_t>> prof;
-    prof.test_insert();
-    prof.print_insert_table();
+    prof.test_all();
     return 0;
 }
 
